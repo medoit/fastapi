4e8a160c5b130d0d8fd87a85b1debcb9fa08167a
medoit <tukaev9413@gmail.com>
2023-06-04 22:33:06 +0300

    add debug in manager

--- auth/manager.py
+++ auth/manager.py
@@ -1,9 +1,10 @@
-from typing import Optional
+from typing import Optional, Union
 
 from fastapi import Depends, Request
-from fastapi_users import BaseUserManager, IntegerIDMixin, models, schemas, exceptions
+from fastapi_users import BaseUserManager, IntegerIDMixin, InvalidPasswordException, models, schemas, exceptions
 
 from auth.database import User, get_user_db
+from auth.schemas import UserCreate
 
 SECRET = "SECRET"
 
@@ -42,17 +43,41 @@ class UserManager(IntegerIDMixin, BaseUserManager[User, int]):
         await self.on_after_register(created_user, request)
 
         return created_user
-    
+#generated token for verify user    
     async def on_after_request_verify(
         self, user: User, token: str, request: Optional[Request] = None
     ):
         print(f"Verification requested for user {user.id}. Verification token: {token}")
-    
+#msg after verify user    
     async def on_after_verify(
         self, user: User, request: Optional[Request] = None
     ):
         print(f"User {user.id} has been verified")
 
+#generated token for reset password      
+    async def on_after_forgot_password(
+        self, user: User, token: str, request: Optional[Request] = None
+    ):
+        print(f"User {user.id} has forgot their password. Reset token: {token}")
+
+
+#valid password        
+    async def validate_password(
+        self,
+        password: str,
+        user: Union[UserCreate, User],
+    ) -> None:
+        if len(password) < 8:
+            raise InvalidPasswordException(
+                reason="Password should be at least 8 characters"
+            )
+        if user.email in password:
+            raise InvalidPasswordException(
+                reason="Password should not contain e-mail"
+            )
+#msg after reset password
+async def on_after_reset_password(self, user: User, request: Optional[Request] = None):
+        print(f"User {user.id} has reset their password.")
 
 async def get_user_manager(user_db=Depends(get_user_db)):
     yield UserManager(user_db)
