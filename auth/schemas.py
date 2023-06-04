8dd74d1debf3ebfb619676bbbd2b568232177786
medoit <tukaev9413@gmail.com>
2023-06-04 18:40:30 +0300

    auth done

---
+++ auth/schemas.py
@@ -0,0 +1,37 @@
+from fastapi_users import schemas
+from typing import Optional
+from datetime import datetime
+
+
+class UserRead(schemas.BaseUser[int]):
+    id: int
+    username: str
+    registered_at: datetime
+    role_id: int
+    email: str
+    is_active: bool = True
+    is_superuser: bool = False
+    is_verified: bool = False
+    
+    class Config:
+        orm_mode = True
+
+
+class UserCreate(schemas.BaseUserCreate):
+    username: str
+    role_id: int
+    email: str
+    password: str
+    is_active: Optional[bool] = True
+    is_superuser: Optional[bool] = False
+    is_verified: Optional[bool] = False
+
+
+class UserUpdate(schemas.BaseUserUpdate):
+    username: str
+    role_id: int
+    password: Optional[str]
+    email: Optional[str]
+    is_active: Optional[bool]
+    is_superuser: Optional[bool]
+    is_verified: Optional[bool]
--- main.py
+++ main.py
@@ -1,46 +1,68 @@
-from typing import List
-from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi import Depends, FastAPI
+from fastapi_users import FastAPIUsers
+
+from auth.database import User
+from auth.auth import auth_backend
+from auth.manager import get_user_manager
+from auth.schemas import UserRead, UserCreate, UserUpdate
 
 
-app = FastAPI(
-    title="PMS-BACKEND"
+fastapi_users = FastAPIUsers[User, int](
+    get_user_manager,
+    [auth_backend],
 )
 
+app = FastAPI(
+    title="FastApi project"
+)
+app.include_router(
+    fastapi_users.get_auth_router(auth_backend), #requires_verification=True
+    prefix="/auth/jwt",
+    tags=["auth"],
+)
 
-@app.get("/")
-def hello():
-    return 'Hello'
+app.include_router(
+    fastapi_users.get_register_router(UserRead, UserCreate),
+    prefix="/auth",
+    tags=["auth"],
+)
 
-users = [
-    {"id": 1, "role": "admin", "name": "Alexander"},
-    {"id": 2, "role": "operator", "name": "Ivan"},
-    {"id": 3, "role": "", "name": ""},
-]
+app.include_router(
+    fastapi_users.get_verify_router(UserRead),
+    prefix="/auth",
+    tags=["auth"],
+)
 
-@app.get("/users/{user_id}")
-def get_user(user_id: int):
-    return [user for user in users if user.get("id") == user_id]
+app.include_router(
+    fastapi_users.get_reset_password_router(),
+    prefix="/auth",
+    tags=["auth"],
+)
 
-@app.get("/users")
-def get_users(limit: int = 10, offset: int = 0):
-    return users[offset:][:limit]
+#This router provides routes to manage users
+app.include_router(
+    fastapi_users.get_users_router(UserRead, UserUpdate),
+    prefix="/users",
+    tags=["users"],
+)
 
-@app.post("/users/{user_id}")
-def change_user(user_id: int, new_name: str):
-    _user = list(filter(lambda user: user.get("id") == user_id, users))[0]
-    _user["name"] = new_name
-    return {"status": 200, "data": _user}
+#Get the current user
+# current_user = fastapi_users.current_user()
 
-list_passwords = []
+# @app.get("/protected-route")
+# def protected_route(user: User = Depends(current_user)):
+#     return f"Hello, {user.email}"
 
-class Password(BaseModel):
-    id: int
-    user_id: int
-    login: str
-    password: str
+#Get the current active and verified user
+current_active_verified_user = fastapi_users.current_user(active=True, verified=True)
 
-@app.post("/passwords")
-def add_password(passwords: List[Password]):
-    list_passwords.extend(passwords)
-    return {"status": 200, "data": list_passwords}
+@app.get("/protected-route")
+def protected_route(user: User = Depends(current_active_verified_user)):
+    return f"Hello, {user.email}"
+
+#Get the current active superuser
+# current_superuser = fastapi_users.current_user(active=True, superuser=True)
+
+# @app.get("/protected-route")
+# def protected_route(user: User = Depends(current_superuser)):
+#     return f"Hello, {user.email}"
